f8ba5b4d33c15264bfe3d17e9f866824637b7b2b
AxelS <axelsoubestte@gmail.com>
2026-04-09 16:24:07 -0300

    Initial Structure

--- Bakery_app/models.py
+++ Bakery_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Catalog(models.Model):
+    product_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.product_name
